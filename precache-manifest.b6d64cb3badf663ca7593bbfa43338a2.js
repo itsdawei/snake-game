81ee52f0248f2a2349126d7f51c9fcfdb849c424
David <dlee20@students.wyischina.com>
2020-07-24 22:42:59 +1000

    Updates

---
+++ precache-manifest.b6d64cb3badf663ca7593bbfa43338a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729897af3a65f0ea0556e2b3036ccc55",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "36933c527db087478dbf",
+    "url": "/snake-game/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a66386437aff8cc25c70",
+    "url": "/snake-game/static/css/main.f1518610.chunk.css"
+  },
+  {
+    "revision": "36933c527db087478dbf",
+    "url": "/snake-game/static/js/2.a0f60c4e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/snake-game/static/js/2.a0f60c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a66386437aff8cc25c70",
+    "url": "/snake-game/static/js/main.4f187855.chunk.js"
+  },
+  {
+    "revision": "0866649720b7e6a02873",
+    "url": "/snake-game/static/js/runtime-main.e42eead7.js"
+  }
+]);
