679ed368aa5fa500bd8a0d951882172db52f7ae9
David <dlee20@students.wyischina.com>
2020-07-26 05:13:42 +1000

    moved Board components to components folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Board from "./Board";
+import Board from "./components/Board";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Badge from "react-bootstrap/Badge";
@@ -27,29 +27,31 @@ class App extends Component {
 
   render() {
     return (
-      <Container fluid className="background">
-        <Row style={{height: "100vh"}}>
-          <Col className="shop-area">
-            <div className="total-food-overlay" />
-            <div className="total-food">
-              Food: <Badge variant="dark">{this.state.score}</Badge>
-            </div>
-            <div className="vl" style={{ right: "0", zIndex: "10" }} />
-          </Col>
-          <Col xs={6} className="game-area">
-            <Board
-              onScoreIncrement={this.incrementScore}
-              onScoreReset={this.resetScore}
-            />
-          </Col>
-          <Col className="third-area">
-            <div
-              className="vl"
-              style={{ left: "0", zIndex: "10", marginLeft: "-2px" }}
-            />
-          </Col>
-        </Row>
-      </Container>
+      <React.Fragment>
+        <Container fluid className="background">
+          <Row style={{ height: "100vh" }}>
+            <Col className="shop-area">
+              <div className="total-food-overlay" />
+              <div className="total-food">
+                Food: <Badge variant="dark">{this.state.score}</Badge>
+              </div>
+              <div className="vl" style={{ right: "0", zIndex: "10" }} />
+            </Col>
+            <Col xs={6} className="game-area">
+              <Board
+                onScoreIncrement={this.incrementScore}
+                onScoreReset={this.resetScore}
+              />
+            </Col>
+            <Col className="third-area">
+              <div
+                className="vl"
+                style={{ left: "0", zIndex: "10", marginLeft: "-2px" }}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </React.Fragment>
     );
   }
 }
