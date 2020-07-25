a161cfd091ef0ee98f031614383b2db907128c90
David <dlee20@students.wyischina.com>
2020-07-26 03:17:45 +1000

    Transmuted the code to use react-bootstrap Grid layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import Board from "./Board";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 import Badge from "react-bootstrap/Badge";
+import { Row, Col } from "react-bootstrap";
 
 class App extends Component {
   constructor(props) {
@@ -26,24 +27,29 @@ class App extends Component {
 
   render() {
     return (
-      <div className="background">
-        <div className="shop-area">
-          <div className="total-food-overlay" />
-          <div className="total-food">
-            Food: <Badge variant="dark">{this.state.score}</Badge>
-          </div>
-          <div className="vl" style={{ right: "0", zIndex: "10" }} />
-        </div>
-        <div className="game-area">
-          <Board
-            onScoreIncrement={this.incrementScore}
-            onScoreReset={this.resetScore}
-          />
-        </div>
-        <div className="third-area">
-          <div className="vl" style={{ left: "0", zIndex: "10", marginLeft: "-2px" }} />
-        </div>
-      </div>
+      <Container fluid className="background">
+        <Row style={{height: "100vh"}}>
+          <Col className="shop-area">
+            <div className="total-food-overlay" />
+            <div className="total-food">
+              Food: <Badge variant="dark">{this.state.score}</Badge>
+            </div>
+            <div className="vl" style={{ right: "0", zIndex: "10" }} />
+          </Col>
+          <Col xs={6} className="game-area">
+            <Board
+              onScoreIncrement={this.incrementScore}
+              onScoreReset={this.resetScore}
+            />
+          </Col>
+          <Col className="third-area">
+            <div
+              className="vl"
+              style={{ left: "0", zIndex: "10", marginLeft: "-2px" }}
+            />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
